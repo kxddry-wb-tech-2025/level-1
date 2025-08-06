eafff276a9f24629b6800ad5c60cfd1598e1b818
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-06 22:59:09 +0300

    20

---
+++ 20/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Reverse(s string) string {
+	words := strings.Split(s, " ")
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	fmt.Println(Reverse("snow dog sun"))
+}
